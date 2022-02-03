f4d6ffee595925edc2c13750c817cca0e0b82b95
lykosn <mitsul.vs@gmail.com>
2022-02-03 16:10:01 +0300

    added models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 USER = 'user'
 MODER = 'moderator'
@@ -23,3 +24,50 @@ class User(AbstractUser):
         choices=ROLES,
         default=USER,
     )
+
+
+class Review(models.Model):
+    text = models.TextField('текст')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    score = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(10)]
+     )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='автор'
+    )
+
+    def __str__(self):
+        return self.text[:15]
+
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
+
+
+class Comment(models.Model):
+    text = models.TextField('текст комментария')
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='автор'
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='отзыв'
+    )
+
+    def __str__(self):
+        return self.text[:15]
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
