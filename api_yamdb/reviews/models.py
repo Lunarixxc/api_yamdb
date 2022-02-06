5aa6374e794a2550a36c5c0261e021690bc6c5a9
Nurlybek.Sharipkhan <zece14zece@yandex.ru>
2022-02-06 19:07:16 +0600

    added categories,titles,genres

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
+from datetime import datetime
 
 USER = 'user'
 MODER = 'moderator'
@@ -12,6 +14,8 @@ ROLES = [
     (ADMIN, 'Админ'),
 ]
 
+def get_year():
+    return datetime.now().year
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -81,3 +85,50 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=256)
+    slug = models.SlugField('Слаг', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=256)
+    slug = models.SlugField('Слаг', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=200)
+    year = models.PositiveSmallIntegerField(
+        'Год выпуска',
+        validators=(MaxValueValidator(get_year),)
+    )
+    description = models.TextField('Описание', blank=True, null=True)
+    genre = models.ManyToManyField(Genre, 'Жанр', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
