24513585bfa8f00721ccdce1d93aa6200d47512f
lykosn <mitsul.vs@gmail.com>
2022-02-08 18:32:47 +0300

    Вроде поправил импорты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from reviews.models import Category, Comment, Genre, Review, Title
 
 from .mixins import CustomViewSet
