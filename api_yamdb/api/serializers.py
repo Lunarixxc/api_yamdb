5a8d80cc518070510b18b297875eda9151cd783a
lykosn <mitsul.vs@gmail.com>
2022-02-08 18:20:01 +0300

    Поправил валидатор и Meta в ReviewSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -12,11 +12,12 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
 
     def validate(self, data):
+        if self.context['request'].method != 'POST':
+            return data
         title_id = self.context['view'].kwargs.get('title_id')
         user = self.context['request'].user
-        if self.context['request'].method == 'POST':
-            if Review.objects.filter(title=title_id, author=user).exists():
-                raise serializers.ValidationError('Вы уже оставляли отзыв!')
+        if Review.objects.filter(title=title_id, author=user).exists():
+            raise serializers.ValidationError('Вы уже оставляли отзыв!')
         return data
 
 
@@ -26,7 +27,7 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = ['id', 'text', 'author', 'pub_date']
+        fields = ('id', 'text', 'author', 'pub_date')
         model = Comment
 
 
